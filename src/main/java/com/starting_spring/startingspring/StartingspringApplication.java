36db05279f58abbef309e574d78d31efa492b084
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-08-15 13:10:04 +0100

    innitial commit

---
+++ src/main/java/com/starting_spring/startingspring/StartingspringApplication.java
@@ -0,0 +1,32 @@
+package com.starting_spring.startingspring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class StartingspringApplication {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("server.port", "8081");
+		SpringApplication.run(StartingspringApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("Lets inspect the beans provided by spring boot");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames){
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
